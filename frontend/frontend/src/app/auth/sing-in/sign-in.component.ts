dd872f510545113af343fa66e5ed7eacf44f90c5
Sylwester312 <sylwester.kowalewski12@gmail.com>
2025-05-03 14:44:08 +0200

    Add sing-in and sing-up

---
+++ frontend/frontend/src/app/auth/sing-in/sign-in.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss'],
+})
+export class SignInComponent {
+  signInForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.signInForm.invalid) return;
+    this.authService.signIn(this.signInForm.value).subscribe({
+      next: (response) => {
+        localStorage.setItem('user', JSON.stringify(response));
+        this.router.navigate(['/tasks']);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+}
---
+++ frontend/frontend/src/app/auth/sing-up/sign-up.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss'],
+})
+export class SignUpComponent {
+  signUpForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.signUpForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.signUpForm.invalid) return;
+    this.authService.signUp(this.signUpForm.value).subscribe({
+      next: (response) => {
+        this.router.navigate(['/auth/sign-in']);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+}
