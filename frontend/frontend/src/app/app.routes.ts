9cbd742173d827f299b2e529ca3d0a0374a4fe01
Sylwester312 <sylwester.kowalewski12@gmail.com>
2025-05-03 16:02:45 +0200

    Add app.routes.ts

--- frontend/frontend/src/app/app.routes.ts
+++ frontend/frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { TaskListComponent } from '../../src/app/auth/tasks/task-detail';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import { CategoryListComponent } from './categories/category-list/category-list.component';
+import { CategoryDetailComponent } from './categories/category-detail/category-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'tasks/:id', component: TaskDetailComponent },
+  { path: 'categories', component: CategoryListComponent },
+  { path: 'categories/:id', component: CategoryDetailComponent },
+  { path: '**', redirectTo: 'sign-in' },
+];
